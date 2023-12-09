6efbad8360b538fed681f2404576e5b6c21ba3c9
longnh257 <hoanglong020597@gmail.com>
2023-12-09 05:31:14 -0500

    first commit

---
+++ database/migrations/2023_12_09_102138_create_s_function_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('s_function', function (Blueprint $table) {
+            $table->string('function_id', 3)->primary();
+            $table->string('function_name', 20);
+            $table->string('category_id', 2);
+            $table->string('url', 256);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('s_function');
+    }
+};
